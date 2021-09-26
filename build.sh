1d14faa6e639a5aaa2358bebb52a4ea65fabacc7
SlaVik <slavakaplya20011501@gmail.com>
2021-09-26 10:42:28 +0300

    rm trash

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 nasm ./bootloader/BootLoader.asm -f bin -o bin/bootloader.bin
-gcc -std=gnu99 -Os -nostdlib -m32 -march=i386 -ffreestanding -o bin/kernel.bin -Wl,--nmagic,--script=com.ld kernel/kernel.c
+gcc -std=gnu99 -Os -nostdlib -m32 -march=i386 -ffreestanding -o bin/kernel.bin -Wl,--nmagic,--script=util/com.ld kernel/kernel.c
 dd if=bin/bootloader.bin of=os.img 
 dd if=bin/kernel.bin of=os.img seek=1 obs=512
