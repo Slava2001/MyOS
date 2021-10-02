32faf8f6d746cb1b2d4b94d3df651736ce6b2329
Slava <slavakaplya20011501@gmail.com>
2021-10-02 12:53:14 +0300

    add an entry to a USB flash drive

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 rm bin/*
 rm os.img
 
-
-
 nasm ./bootloader/BootLoader.asm -f bin -o bin/bootloader.bin
 
 bcc -Iinclude -W -0 -c kernel/kernel.c -o bin/kernel.obj
@@ -13,6 +11,10 @@ bcc -Iinclude -W -0 -c lib/memory.c -o bin/memory.obj
 
 ld86 -d bin/*.obj -o bin/os.bin
 
-# gcc -std=gnu99 -Os -nostdlib -m32 -march=i386 -ffreestanding -o bin/kernel.bin -Wl,--nmagic,--script=util/com.ld kernel/kernel.c
-dd if=bin/bootloader.bin of=os.img 
-dd if=bin/os.bin of=os.img seek=1 obs=512
+dd if=bin/bootloader.bin of=os.img obs=1 count=3 iflag=skip_bytes,count_bytes
+dd if=bin/bootloader.bin of=os.img obs=1 skip=62 seek=62 iflag=skip_bytes,count_bytes
+dd if=bin/os.bin of=os.img seek=1 obs=512 
+
+# sudo dd if=bin/bootloader.bin of=/dev/sdb obs=1 count=3 iflag=skip_bytes,count_bytes
+# sudo dd if=bin/bootloader.bin of=/dev/sdb obs=1 skip=62 seek=62 iflag=skip_bytes,count_bytes
+# sudo dd if=bin/os.bin of=/dev/sdb seek=1 obs=512 
