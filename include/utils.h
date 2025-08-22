ab683033ba6b853f12e9282912f211c4a3de24ea
Slava2001 <slavakaplya20011501@gmail.com>
2025-08-22 17:53:44 +0300

    fix logs

--- include/utils.h
+++ include/utils.h
@@ -5,18 +5,37 @@
 
 extern char *APP_NAME;
 
-#if 0
-#define log(lvl, msg) {printf(__FILE__":%d %s["#lvl"]: ", __LINE__, APP_NAME); printf msg; puts("\n\r");}
-#else
-#define log(lvl, msg) {puts(APP_NAME); puts("["#lvl"]: "); printf msg; puts("\n\r");}
+#define Dbg 0
+#define Info 1
+#define Err 2
+#define Disable 4
+
+#ifndef log_lvl
+#define log_lvl Dbg
 #endif
 
-#define logi(msg) log(Info, msg)
-#define loge(msg) log(Err,  msg)
-#ifdef DEBUG
-#define logd(msg) log(Dbg,  msg)
+#if log_lvl == Dbg
+#define log(lvl, msg) {printf(__FILE__":%d %s["lvl"]: ", __LINE__, APP_NAME); printf msg; puts("\n\r");}
+#else
+#define log(lvl, msg) {puts(APP_NAME); puts("["lvl"]: "); printf msg; puts("\n\r");}
+#endif
+
+#if Dbg >= log_lvl
+#define logd(msg) log("Dbg ", msg)
 #else
 #define logd(msg)
+#endif
+
+#if Info >= log_lvl
+#define logi(msg) log("Info", msg)
+#else
+#define logi(msg)
+#endif
+
+#if Err >= log_lvl
+#define loge(msg) log("Err ", msg)
+#else
+#define loge(msg)
 #endif
 
 #define rci(cond, code, msg) if (cond) { loge(msg); return code; }
